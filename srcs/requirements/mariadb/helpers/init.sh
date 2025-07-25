18a164a9f77a557f1abeeb7592415bd0e6b71320
Kamal El Alami <kelalami123@gmail.com>
2025-07-25 13:35:40 +0100

    db problem

--- srcs/requirements/mariadb/helpers/init.sh
+++ srcs/requirements/mariadb/helpers/init.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Start MariaDB
 service mariadb start
+
+# Create database and user
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;" 
 mysql -u root -e "CREATE USER IF NOT EXISTS \`$DB_USER\`@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO \`$DB_USER\`@'%';"
 mysql -u root -e "FLUSH PRIVILEGES;"
+
+mysqld
---
+++ srcs/requirements/wordpress/helpers/init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Create PHP-FPM socket directory
+mkdir -p /run/php
+chown -R www-data:www-data /run/php/
+
+
+
+# Setup WordPress if not already configured
+if [ ! -f /var/www/html/wp-config.php ]; then
+    wp core download --allow-root --path=/var/www/html
+    wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root --path=/var/www/html
+    wp core install --url=$WP_HOST --title="$WP_TITLE" --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root --path=/var/www/html
+    wp user create $WP_USER2 $WP_EMAIL2 --role=author --user_pass=$WP_PASS2 --allow-root --path=/var/www/html
+fi
+
+# Start PHP-FPM
+php-fpm7.4 -F
