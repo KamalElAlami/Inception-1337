0105675df96d95ae0d3708fa4af058c7db392c25
Kamal El Alami <kelalami123@gmail.com>
2025-08-05 20:38:37 +0100

    configuering cgi config file to accept connection from 0.0.0.0:3000

--- srcs/requirements/wordpress/helpers/init.sh
+++ srcs/requirements/wordpress/helpers/init.sh
@@ -4,7 +4,7 @@
 mkdir -p /run/php
 chown -R www-data:www-data /run/php/
 
-
+sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.4/fpm/pool.d/www.conf
 
 # Setup WordPress if not already configured
 if [ ! -f /var/www/html/wp-config.php ]; then
