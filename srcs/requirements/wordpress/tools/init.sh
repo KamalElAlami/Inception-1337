1c8c4b3561dca44f2c8459a941ef4f0e8476d71e
Kamal El Alami <kelalami123@gmail.com>
2025-09-08 19:48:56 +0100

    redis

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -8,8 +8,11 @@ sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.
 if [ ! -f /var/www/html/wp-config.php ]; then
     wp core download --allow-root --path=/var/www/html
     wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root --path=/var/www/html
+    wp config set WP_REDIS_HOST redis --allow-root
+  	wp config set WP_REDIS_PORT 6380 --raw --allow-root
     wp core install --url=$WP_HOST --title="$WP_TITLE" --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root --path=/var/www/html
     wp user create $WP_USER2 $WP_EMAIL2 --role=author --user_pass=$WP_PASS2 --allow-root --path=/var/www/html
+    wp plugin install redis-cache --activate --allow-root
 fi
 
 # Start PHP-FPM
