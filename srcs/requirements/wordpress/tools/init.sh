410aad3b8d3f1ed4f82f597732a8eb909e60373e
Kamal El Alami <kelalami123@gmail.com>
2025-09-13 13:59:57 +0100

    bonus is done

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -2,7 +2,6 @@
 
 mkdir -p /run/php
 chown -R www-data:www-data /run/php/
-chown -R www-data:www-data /var/www/html/wp-content
 
 sed -i 's|listen = /run/php/php7.4-fpm.sock|listen = 0.0.0.0:9000|g' /etc/php/7.4/fpm/pool.d/www.conf
 
@@ -11,10 +10,12 @@ if [ ! -f /var/www/html/wp-config.php ]; then
     wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root --path=/var/www/html
     wp config set WP_REDIS_HOST redis --allow-root
   	wp config set WP_REDIS_PORT 6380 --raw --allow-root
+    wp config set FS_METHOD 'direct' --allow-root
     wp core install --url=$WP_HOST --title="$WP_TITLE" --admin_user=$WP_USER --admin_password=$WP_PASS --admin_email=$WP_EMAIL --allow-root --path=/var/www/html
     wp user create $WP_USER2 $WP_EMAIL2 --role=author --user_pass=$WP_PASS2 --allow-root --path=/var/www/html
     wp plugin install redis-cache --activate --allow-root
-    wp redis-cache enable --allow-root
+    wp redis enable --allow-root
+    chown -R www-data:www-data /var/www/html/wp-content/
 fi
 
 # Start PHP-FPM
